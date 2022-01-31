8bbb8e0b333d4934b37ee723cd8bf6708b560472
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2022-01-31 23:56:01 +0530

    Create Find Missing And Repeating .cpp

---
+++ dsa450/searching&sorting/Find Missing And Repeating .cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        int* ans = new int[2];
+        unordered_set<int> s;
+        for(int i = 0; i < n; i++){
+            if(s.find(arr[i]) != s.end()){
+                ans[0] = arr[i];
+            }
+            else{
+                s.insert(arr[i]);
+            }
+        }
+        
+        int first = 1;
+        
+        for(int i = 1; i < n+1; i++){
+            if(s.find(i) == s.end()){
+                ans[1] = i;
+                break;
+            }
+        }
+        
+        return ans;
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
