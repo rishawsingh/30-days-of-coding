de457e651ab94a43b539785ed4c6711ea7e21c01
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2022-02-02 01:28:52 +0530

    Create 3. Insertion Sort .cpp

---
+++ data structure basic/sorting/3. Insertion Sort .cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+void insertionSort(int arr[], int n);
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* The task is to complete insert() which is used 
+   as shown below to implement insertionSort() */
+
+
+    void insert(int arr[], int n)
+    {
+        int i,j,key;
+        // Your code here 
+        for(i=1;i<n;i++)
+        {
+            key=arr[i];
+            j=i-1;
+            while(j>=0 && arr[j]>key)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        insert(arr,n);
+        //code here
+    }
+
+
+
+
